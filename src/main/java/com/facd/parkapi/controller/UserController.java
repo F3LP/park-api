fff5e41624f94da9d50df0f62a29bb7f46bd500d
Felipe Dantas <felipeacdantas@gmail.com>
2024-01-04 14:33:46 -0300

    Added feature to create user

--- src/main/java/com/facd/parkapi/controller/UserController.java
+++ src/main/java/com/facd/parkapi/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.facd.parkapi.controller;
 
+import com.facd.parkapi.entity.User;
 import com.facd.parkapi.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity <User> createUser(@RequestBody User user){
+        User userCreated = userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+
 }
--- src/main/java/com/facd/parkapi/service/UserService.java
+++ src/main/java/com/facd/parkapi/service/UserService.java
@@ -1,12 +1,19 @@
 package com.facd.parkapi.service;
 
+import com.facd.parkapi.entity.User;
 import com.facd.parkapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }
